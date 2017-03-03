7dff6596494cccbc0a63f1459ac92f1e068a616b
Junyoung Choi <fluke8259@gmail.com>
2017-03-04 05:56:28 +0900

    Handle Katex parse error

--- packages/rehype-katex/index.js
+++ packages/rehype-katex/index.js
@@ -2,6 +2,7 @@ const visit = require('unist-util-visit')
 const katex = require('katex')
 const unified = require('unified')
 const parse = require('rehype-parse')
+const position = require('unist-util-position')
 
 function parseMathHtml (html) {
   return unified()
@@ -24,7 +25,11 @@ module.exports = function plugin (opts = {}) {
           if (opts.throwOnError) {
             throw err
           } else {
-
+            file.message(
+              err.message,
+              position.start(element)
+            )
+            renderedValue = katex.renderToString(element.children[0].value, {throwOnError: false, errorColor: opts.errorColor}, 'katex-parse-error')
           }
         }
 
@@ -44,7 +49,11 @@ module.exports = function plugin (opts = {}) {
           if (opts.throwOnError) {
             throw err
           } else {
-
+            file.message(
+              err.message,
+              position.start(element)
+            )
+            renderedValue = katex.renderToString(element.children[0].value, {throwOnError: false, errorColor: opts.errorColor}, 'katex-parse-error')
           }
         }
         const mathAst = parseMathHtml(renderedValue).children[0]
--- specs/rehype-katex.spec.js
+++ specs/rehype-katex.spec.js
@@ -10,7 +10,7 @@ const toHtml = require('hast-util-to-html')
 
 function remark () {
   return unified()
-    .use(parse, {position: false})
+    .use(parse)
 }
 
 function parseHtml (html) {
@@ -47,4 +47,48 @@ it('should parse into katex', () => {
   expect(renderedAst.children[2].tagName).toEqual('div')
   expect(renderedAst.children[2].properties.className).toEqual(expect.arrayContaining(['math']))
   expect(toHtml(renderedAst.children[2].children[0], {fragment: true})).toEqual(expectedMath)
+})
+
+it('should handle error', () => {
+  const processor = remark()
+    .use(math)
+    .use(remark2rehype)
+    .use(rehypeKatex, {
+      errorColor: 'orange'
+    })
+    .use(stringify)
+
+  const targetText = '$\\alpa$'
+
+  const result = processor.processSync(targetText)
+  const renderedAst = parseHtml(result.toString())
+
+  const expectedInlineMath = katex.renderToString('\\alpa', {
+    throwOnError: false,
+    errorColor: 'orange'
+  })
+
+  expect(renderedAst.children[0].type).toEqual('element')
+  expect(renderedAst.children[0].tagName).toEqual('p')
+  expect(renderedAst.children[0].children[0].tagName).toEqual('span')
+  expect(renderedAst.children[0].children[0].properties.className).toEqual(expect.arrayContaining(['inlineMath']))
+  expect(toHtml(renderedAst.children[0].children[0].children[0], {fragment: true})).toEqual(expectedInlineMath)
+
+  expect(result.messages[0].message).toEqual('KaTeX parse error: Expected \'EOF\', got \'\\alpa\' at position 1: \\̲a̲l̲p̲a̲')
+})
+
+it('should throw parsing error if `throwOnError` set true', () => {
+  const processor = remark()
+    .use(math)
+    .use(remark2rehype)
+    .use(rehypeKatex, {
+      throwOnError: true
+    })
+    .use(stringify)
+
+  const targetText = '$\\alpa$'
+
+  expect(() => {
+    processor.processSync(targetText)
+  }).toThrow('KaTeX parse error: Expected \'EOF\', got \'\\alpa\'')
 })
