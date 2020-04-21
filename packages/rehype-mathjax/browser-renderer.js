6c4c83e61de27f13925083b51ff7d794f28ad544
TANIGUCHI MASAYA <mew@cat.ovh>
2020-04-21 19:05:49 +0900

    Update browser-renderer

--- packages/rehype-mathjax/browser-renderer.js
+++ packages/rehype-mathjax/browser-renderer.js
@@ -2,30 +2,61 @@ const fromDom = require('hast-util-from-dom')
 const {mathjax} = require('mathjax-full/js/mathjax')
 const {TeX} = require('mathjax-full/js/input/tex')
 const {SVG} = require('mathjax-full/js/output/svg')
+const {CHTML} = require('mathjax-full/js/output/chtml')
 const {browserAdaptor} = require('mathjax-full/js/adaptors/browserAdaptor')
 const {RegisterHTMLHandler} = require('mathjax-full/js/handlers/html')
 const {AllPackages} = require('mathjax-full/js/input/tex/AllPackages')
 
-exports.styleSheet = styleSheet
-exports.render = render
+class AbstractRenderer {
+  constructor() {
+    this.adaptor = browserAdaptor()
+    RegisterHTMLHandler(this.adaptor)
+  }
 
-const adaptor = browserAdaptor()
-RegisterHTMLHandler(adaptor)
-const tex = new TeX({packages: AllPackages})
-const svg = new SVG({fontCache: 'none'})
-const mathDocument = mathjax.document('', {InputJax: tex, OutputJax: svg})
+  get styleSheet() {
+    return {
+      type: 'element',
+      tagName: 'style',
+      properties: {},
+      children: [
+        {
+          type: 'text',
+          value: this.adaptor.textContent(
+            this.OutputJax.styleSheet(this.mathDocument)
+          )
+        }
+      ]
+    }
+  }
 
-function styleSheet() {
-  return {
-    type: 'element',
-    tagName: 'style',
-    properties: {},
-    children: [
-      {type: 'text', value: adaptor.textContent(svg.styleSheet(mathDocument))}
-    ]
+  render(latex, options) {
+    return fromDom(this.mathDocument.convert(latex, options))
   }
 }
 
-function render(math, options) {
-  return fromDom(mathDocument.convert(math, options))
+class SVGRenderer extends AbstractRenderer {
+  constructor(options) {
+    super()
+    this.InputJax = new TeX({packages: AllPackages})
+    this.OutputJax = new SVG(options)
+    this.mathDocument = mathjax.document('', {
+      InputJax: this.InputJax,
+      OutputJax: this.OutputJax
+    })
+  }
 }
+
+class CHTMLRenderer extends AbstractRenderer {
+  constructor(options) {
+    super()
+    this.InputJax = new TeX({packages: AllPackages})
+    this.OutputJax = new CHTML(options)
+    this.mathDocument = mathjax.document('', {
+      InputJax: this.InputJax,
+      OutputJax: this.OutputJax
+    })
+  }
+}
+
+exports.SVGRenderer = SVGRenderer
+exports.CHTMLRenderer = CHTMLRenderer
