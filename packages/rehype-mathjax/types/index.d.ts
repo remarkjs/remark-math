e1e0a6b18dd28bdc5aa9636b54b648ea1ef228e2
Yasuaki Uechi <y@uechi.io>
2020-07-07 14:47:13 +0900

    feat: add type definition for rehype-mathjax

---
+++ packages/rehype-mathjax/types/index.d.ts
@@ -0,0 +1,25 @@
+import {Plugin} from 'unified'
+
+// should be ported back to MathJax repo
+// http://docs.mathjax.org/en/latest/options/output/svg.html#the-configuration-block
+interface MathJaxSVGOptions {
+  scale: number
+  minScale: number
+  mtextInheritFont: boolean
+  merrorInheritFont: boolean
+  mathmlSpacing: boolean
+  skipAttributes: {[index: string]: boolean}
+  exFactor: number
+  displayAlign: 'left' | 'center' | 'right'
+  displayIndent: string
+  fontCache: 'local' | 'global'
+  localID: string | null
+  internalSpeechTitles: true
+  titleID: number
+}
+
+type RenderSVGOptions = Partial<MathJaxSVGOptions>
+
+declare const renderSvg: Plugin<[RenderSVGOptions?]>
+
+export = renderSvg
