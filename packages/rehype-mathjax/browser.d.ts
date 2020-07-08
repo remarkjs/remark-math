316573939a0e84f358d995a6ab69ec6c2db61152
Yasuaki Uechi <y@uechi.io>
2020-07-08 16:18:32 +0900

    fix: support chtml and browser submodule

---
+++ packages/rehype-mathjax/browser.d.ts
@@ -0,0 +1,12 @@
+import {Plugin} from 'unified'
+
+// http://docs.mathjax.org/en/latest/options/input/tex.html#the-configuration-block
+type MathNotation = [string, string]
+interface BrowserOptions {
+  displayMath: MathNotation | MathNotation[]
+  inlineMath: MathNotation | MathNotation[]
+}
+
+declare const renderBrowser: Plugin<[Partial<BrowserOptions>?]>
+
+export = renderBrowser
