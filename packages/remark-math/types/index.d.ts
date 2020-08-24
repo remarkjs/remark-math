5b7049cef4608890be45d377103e2e18b3b7d7ab
Rongjian Zhang <pd4d10@gmail.com>
2020-08-24 23:16:43 +0800

    Update packages/remark-math/types/index.d.ts
    Co-authored-by: Christian Murphy <christian.murphy.42@gmail.com>

--- packages/remark-math/types/index.d.ts
+++ packages/remark-math/types/index.d.ts
@@ -1,11 +1,11 @@
 import {Plugin} from 'unified' // eslint-disable-line import/no-extraneous-dependencies
 
 declare namespace remarkMath {
-  type Math = Plugin<[RemarkMathOptions?]>
-
   interface RemarkMathOptions {
     inlineMathDouble?: boolean
   }
+
+  type Math = Plugin<[RemarkMathOptions?]>
 }
 
 declare const remarkMath: remarkMath.Math
