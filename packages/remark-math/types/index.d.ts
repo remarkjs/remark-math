415e180c48d7a73c3977f45dc5fd4fd556ec32d5
Rongjian Zhang <pd4d10@gmail.com>
2020-08-25 16:22:02 +0800

    remark-math: expose options type
    Closes GH-51.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

--- packages/remark-math/types/index.d.ts
+++ packages/remark-math/types/index.d.ts
@@ -1,9 +1,13 @@
 import {Plugin} from 'unified' // eslint-disable-line import/no-extraneous-dependencies
 
-interface RemarkMathOptions {
-  inlineMathDouble?: boolean
+declare namespace remarkMath {
+  interface RemarkMathOptions {
+    inlineMathDouble?: boolean
+  }
+
+  type Math = Plugin<[RemarkMathOptions?]>
 }
 
-declare const math: Plugin<[RemarkMathOptions?]>
+declare const remarkMath: remarkMath.Math
 
-export = math
+export = remarkMath
