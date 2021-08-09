0b77136996d1eaea2db1aa745fef0a058424ce6e
Titus Wormer <tituswormer@gmail.com>
2021-08-09 13:08:34 +0200

    Remove warning for remark 12

--- packages/remark-math/index.js
+++ packages/remark-math/index.js
@@ -7,9 +7,6 @@
 import {math} from 'micromark-extension-math'
 import {mathFromMarkdown, mathToMarkdown} from 'mdast-util-math'
 
-/** @type {boolean|undefined} */
-let warningIssued
-
 /**
  * Plugin to support math.
  *
@@ -17,23 +14,6 @@ let warningIssued
  */
 export default function remarkMath() {
   const data = this.data()
-
-  // Old remark.
-  /* c8 ignore next 14 */
-  if (
-    !warningIssued &&
-    ((this.Parser &&
-      this.Parser.prototype &&
-      this.Parser.prototype.blockTokenizers) ||
-      (this.Compiler &&
-        this.Compiler.prototype &&
-        this.Compiler.prototype.visitors))
-  ) {
-    warningIssued = true
-    console.warn(
-      '[remark-math] Warning: please upgrade to remark 13 to use this plugin'
-    )
-  }
 
   add('micromarkExtensions', math)
   add('fromMarkdownExtensions', mathFromMarkdown)
